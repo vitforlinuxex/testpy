5b240dd13eb8947e8aed33851f5970721e18a964
vitforlinuxex <vitforlinux.extra@gmail.com>
2025-10-22 17:52:53 +0200

    Add files via upload

---
+++ domande.py
@@ -0,0 +1,19 @@
+print("Domanda")
+risposta = input()
+c = 0
+
+if risposta == "34":
+    print ("no")
+elif risposta == "43":
+    c=c+1
+
+print("Domanda")
+risposta = input()
+
+
+if risposta == "34":
+    print ("no")
+elif risposta == "43":
+    c=c+1
+    
+print (c)
